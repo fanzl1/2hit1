e92a0e2dde2569eb9a1740ff309d2376a3f3e33c
fanzhenli <fanzhenli@192.168.1.112>
2015-03-25 00:07:56 +0800

    整合代码

--- 2hit1/gen/com/example/hit1/R.java
+++ 2hit1/gen/com/example/hit1/R.java
@@ -10,6 +10,22 @@ package com.example.hit1;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f040000;
+        public static final int line1=0x7f040001;
+        public static final int line2=0x7f040002;
+        public static final int player1=0x7f040005;
+        public static final int player1_click=0x7f040003;
+        public static final int player2=0x7f040006;
+        public static final int player2_click=0x7f040004;
+        public static final int text=0x7f040007;
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
     public static final class drawable {
         public static final int account=0x7f020000;
         public static final int creatafterclick=0x7f020001;
@@ -26,21 +42,26 @@ public final class R {
         public static final int untitle=0x7f02000c;
     }
     public static final class id {
-        public static final int edittext1=0x7f050003;
-        public static final int edittext2=0x7f050004;
-        public static final int imageLogin=0x7f050006;
-        public static final int imageReg=0x7f050005;
-        public static final int imageView1=0x7f050002;
-        public static final int imageView2=0x7f050001;
-        public static final int relativeLayout1=0x7f050000;
+        public static final int action_restart=0x7f080007;
+        public static final int edittext1=0x7f080003;
+        public static final int edittext2=0x7f080004;
+        public static final int imageLogin=0x7f080006;
+        public static final int imageReg=0x7f080005;
+        public static final int imageView1=0x7f080002;
+        public static final int imageView2=0x7f080001;
+        public static final int relativeLayout1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
+    public static final class menu {
+        public static final int main=0x7f070000;
+    }
     public static final class string {
-        public static final int Sdr=0x7f040003;
-        public static final int app_name=0x7f040000;
-        public static final int desc=0x7f040002;
-        public static final int hello_world=0x7f040001;
+        public static final int Sdr=0x7f060003;
+        public static final int action_restart=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int desc=0x7f060002;
+        public static final int hello_world=0x7f060001;
     }
 }
--- 2hit1/src/com/example/hit1/MainActivity.java
+++ 2hit1/src/com/example/hit1/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.hit1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
 	
-	
+	private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        iv = (ImageView)findViewById(R.id.imageLogin);
+        
+        iv.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this,GameActivity.class);
+				startActivity(intent);
+			}
+		});
         
     }
 }
